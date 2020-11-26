311de2f98da823b0cecc954787e1a48bdd8fab8d
salehanoor644 <salehanoor896@gmail.com>
2020-11-26 01:47:36 -0800

    button

--- lib/reUseableCardWidgets.dart
+++ lib/reUseableCardWidgets.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ReuseableContainer extends StatelessWidget {
-  final Color colorr; //Color(0xFF1D1E33),
+  final Color colorr; //Color(0xFF1D1E23),
   final Widget cardWidget;
   final Function onPress;
   ReuseableContainer({@required this.colorr, this.cardWidget, this.onPress});
