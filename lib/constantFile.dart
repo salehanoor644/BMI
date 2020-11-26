c09cae895aad55d40212b6b1c6ec60f1bbac93e2
salehanoor644 <salehanoor896@gmail.com>
2020-11-26 01:40:32 -0800

    button

--- lib/constantFile.dart
+++ lib/constantFile.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const kactiveColor = Color(0xFF1D1E33);
 const kinactiveColor = Color(0xFF111320);
 const kBottomContainerHeight = 50.0;
-const kBottomContainerColour = Colors.lightGreen;
+const kBottomContainerColour = Colors.yellow;
 
 const kbottemContainerStyle = TextStyle(
   fontSize: 30.0,
